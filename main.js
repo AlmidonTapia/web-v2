11e84b934dfc8db66b38fa137afde5d652e67e52
saultapia24 <almts.04@gmail.com>
2023-11-14 20:25:47 -0500

    deploy

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+  document.querySelector(".nav-menu").classList.toggle("show");
+});
+ScrollReveal().reveal('.provincias', { delay:500 });
+ScrollReveal().reveal('.showcase', { delay:500 });
+ScrollReveal().reveal('.news-cards', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
+
